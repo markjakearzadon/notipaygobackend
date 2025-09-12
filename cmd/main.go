ae1eca13b2e5dc9117b6db434ee11ffa4854691a
markjake <markjakearzadon@gmail.com>
2025-09-12 19:22:45 +0800

    it's weerking

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,49 @@
-main
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/markjakearzadon/notipay-gobackend.git/internal/db"
+	"github.com/markjakearzadon/notipay-gobackend.git/internal/handlers"
+	"github.com/markjakearzadon/notipay-gobackend.git/internal/services"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("error loading .env %s", err)
+	}
+
+	uri := os.Getenv("MONGOURI")
+	if err := db.Connect(uri); err != nil {
+		log.Fatalf("failed to connect %s", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	defer db.Disconnect(ctx)
+
+	err = db.Client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	notidatabase := db.Client.Database("notipaydb")
+	userService := services.NewUserService(notidatabase)
+	userHandler := handlers.NewUserHandler(userService)
+
+	// Set up router
+	router := mux.NewRouter()
+	router.HandleFunc("/users", userHandler.CreateUser).Methods("POST")
+	router.HandleFunc("/users", userHandler.GetUsers).Methods("GET")
+
+	// Start server
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
