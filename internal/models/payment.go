1100c64547f712703ec72137e7a6f6ef1a5b59e3
markjake <markjakearzadon@gmail.com>
2025-09-27 00:02:47 +0800

    ..

--- internal/models/payment.go
+++ internal/models/payment.go
@@ -2,20 +2,22 @@ package models
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Payment struct {
-	ID             string    `bson:"_id,omitempty" json:"id"`
-	ReferenceID    string    `bson:"reference_id" json:"reference_id"`
-	PayerID        string    `bson:"payer_id" json:"payer_id"`
-	PayeeID        string    `bson:"payee_id" json:"payee_id"`
-	Amount         float64   `bson:"amount" json:"amount"`
-	Title          string    `bson:"title" json:"title"`             // Payment title
-	Description    string    `bson:"description" json:"description"` // Payment description
-	Status         string    `bson:"status" json:"status"`           // e.g., "PENDING", "COMPLETED", "FAILED"
-	ChargeID       string    `bson:"charge_id" json:"charge_id"`
-	DisbursementID string    `bson:"disbursement_id" json:"disbursement_id"`
-	CheckoutURL    string    `bson:"checkout_url" json:"checkout_url"` // For frontend redirect
-	CreatedAt      time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt      time.Time `bson:"updated_at" json:"updated_at"`
+	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	ReferenceID    string             `bson:"reference_id" json:"reference_id"`
+	PayerID        string             `bson:"payer_id" json:"payer_id"`
+	PayeeID        string             `bson:"payee_id" json:"payee_id"`
+	Amount         float64            `bson:"amount" json:"amount"`
+	Title          string             `bson:"title" json:"title"`             // Payment title
+	Description    string             `bson:"description" json:"description"` // Payment description
+	Status         string             `bson:"status" json:"status"`           // e.g., "PENDING", "COMPLETED", "FAILED"
+	ChargeID       string             `bson:"charge_id" json:"charge_id"`
+	DisbursementID string             `bson:"disbursement_id" json:"disbursement_id"`
+	CheckoutURL    string             `bson:"checkout_url" json:"checkout_url"` // For frontend redirect
+	CreatedAt      time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt      time.Time          `bson:"updated_at" json:"updated_at"`
 }
